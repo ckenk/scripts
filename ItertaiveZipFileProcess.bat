5c4d061fb6466721e14c8e8f4152fdef29b1eac4
ckenk <kenneth.kulathilake@gmail.com>
2022-03-27 17:45:16 -0400

    Update ItertaiveZipFileProcess.bat
    Description added.

--- ItertaiveZipFileProcess.bat
+++ ItertaiveZipFileProcess.bat
@@ -3,7 +3,11 @@
 @REM http://stackoverflow.com/questions/2396003/get-parent-directory-name-for-a-particular-file-using-dos-batch-scripting
 
 
-@REM Unzip the zip file to a folder havaing the name of the zip file
+@REM Unzip multiple zip files, each to a folder havaing the same name of the zip file.
+@REM Content of the zip file will be in the folder and then another script can be run to process each.
+@REM In this case, the files inside the folders are expected to be files with '.log' extension and 
+@REM their names are passed in to a another script to be processed.
+@REM
 @REM %~dI - expands %I to a drive letter only
 @REM %~pI - expands %I to a path only
 @REM %~nI - expands %I to a file name only
